e79e4689a77c145dfd584d86d300e491fb6ed396
Nortalle <vincent.guidoux@gmail.com>
2026-05-28 16:32:40 +0200

    add devcontainer configuration and post-create script for Laravel Livewire setup

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+## Configure Bash
+# Aliases for common commands
+tee -a ~/.bash_aliases > /dev/null <<"EOF"
+alias tree='tree --dirsfirst -A -F'
+EOF
+
+## Enable globstar option for recursive globbing
+tee -a ~/.bashrc > /dev/null <<"EOF"
+shopt -s globstar nullglob
+EOF
+
+## Install required packages
+# Update packages list
+sudo apt update
+
+# Install common packages
+sudo apt install --yes neovim
+
+# Install network utilities (dig, nslookup, ping, ncat)
+sudo apt install --yes dnsutils iputils-ping ncat
+
+# Install PHP and Composer
+sudo apt install --yes php-common php-cli php-mysql php-xml php-bcmath composer
+
+## Install Laravel Installer
+composer global require laravel/installer
+
+# Add Composer global bin to PATH
+tee -a ~/.bashrc > /dev/null <<"EOF"
+export PATH="${HOME}/.composer/vendor/bin:${PATH}"
+EOF
